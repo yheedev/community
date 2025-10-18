32bf919d60548ab0f5662fd43449ec58ab6e9389
yheedev <yheedev@gmail.com>
2025-10-18 14:49:14 +0900

    chore: nativeWind, alias setting

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,14 @@
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
+
 const config = getDefaultConfig(__dirname);
+
+config.resolver.alias = {
+  "@": path.resolve(__dirname),
+};
+
 module.exports = config;
+
+// const { getDefaultConfig } = require("expo/metro-config");
+// const config = getDefaultConfig(__dirname);
+// module.exports = config;
