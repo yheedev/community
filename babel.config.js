c71d863aadd6e30106e82ef9b8418a03fcd26dda
yheedev <yheedev@gmail.com>
2025-10-19 02:15:19 +0900

    chore: setting expo router, aliases, zustand

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel", "react-native-reanimated/plugin"],
+    plugins: ["expo-router/babel", ["module-resolver", { alias: { "@": "./" } }]],
   };
 };
